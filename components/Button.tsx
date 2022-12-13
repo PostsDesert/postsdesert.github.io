7aec6ea27a74ee8d98d38679d85e06afa758a5d1
Griffith Baker <gmbaker3@ncsu.edu>
2022-12-13 23:15:58 +0100

    use <a> tags for button links to allow copying link

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Styles from '../styles/Button.module.css';
 
@@ -6,14 +5,15 @@ import Styles from '../styles/Button.module.css';
 const Button = ({svgSrc, svgAlt, text, className, textColor, buttonColor, link}) => {
     const isInternalLink = link.startsWith('#');
     return (
+      <a href={link} target={isInternalLink ? '_self' : '_blank'} rel="noreferrer">
       <button 
         className={`${Styles.button} ${className}`} 
         style={{backgroundColor: buttonColor, color: textColor}}
-        onClick={() => isInternalLink ? window.open(link, '_self') : window.open(link, '_blank')}
         type='button'>
         <span className={Styles.buttonText}>{text}</span>
         <img className={Styles.buttonIcon} style={{height:'1.2em'}} src={svgSrc} alt={svgAlt} />
       </button>
+      </a>
     )
 }
 
